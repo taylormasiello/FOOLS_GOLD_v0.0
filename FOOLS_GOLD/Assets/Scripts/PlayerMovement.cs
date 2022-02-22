3ad21a7432cd8249a4b545528fa9021816c779c1
Taylor Masiello <masiellotay@gmail.com>
2022-02-22 13:45:25 -0800

    player movement functionality created, animations not connected

---
+++ FOOLS_GOLD/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
