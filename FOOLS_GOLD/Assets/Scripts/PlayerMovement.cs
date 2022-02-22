1357f962ca87ea99b18ac55c3194646481be3cb0
Taylor Masiello <masiellotay@gmail.com>
2022-02-22 14:21:50 -0800

    player movement input system and animations created, implemented, and functioning; collider bug fixes

--- FOOLS_GOLD/Assets/Scripts/PlayerMovement.cs
+++ FOOLS_GOLD/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
 
     Vector2 movement;
 
@@ -14,6 +15,10 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
