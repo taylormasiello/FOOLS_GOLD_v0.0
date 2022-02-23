2f44b07e2ba128286a027c5a0dac2260d5e9a6bd
Taylor Masiello <masiellotay@gmail.com>
2022-02-22 21:02:37 -0800

    wall bump bug fix

--- FOOLS_GOLD/Assets/Scripts/MineAction.cs
+++ FOOLS_GOLD/Assets/Scripts/MineAction.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MineAction : MonoBehaviour
 {
+    [SerializeField] CapsuleCollider2D capsuleCollider;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Kenny is trying to mine");
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Mining"))) 
+        {
+            return;
+        }
+        else
+        {
+            Debug.Log("Kenny is trying to mine");
+        }
+
+        
     }
 
-    void OnCollisionExit2D(Collision2D collision) // placeholder, might not need
-    {
-        Debug.Log("Kenny is finished mining");
-    }
+    //void OnCollisionExit2D(Collision2D mining) // placeholder, prob won't need
+    //{
+    //    if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
+    //    {
+    //        Debug.Log("Kenny is finished mining");
+    //    }        
+    //}
 }
