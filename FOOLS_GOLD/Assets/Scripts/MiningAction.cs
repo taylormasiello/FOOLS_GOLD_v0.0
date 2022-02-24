6989f9135a98497e606aeb5f36f71a01d320f519
Taylor Masiello <masiellotay@gmail.com>
2022-02-24 01:54:55 -0800

    equip pickaxe functionality implemented without bugs

--- FOOLS_GOLD/Assets/Scripts/MiningAction.cs
+++ FOOLS_GOLD/Assets/Scripts/MiningAction.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class MiningAction : MonoBehaviour
 {
     [SerializeField] GameObject gameCursor;
-    [SerializeField] GameObject backpack;
+    [SerializeField] Button backpackBtn;
 
     [SerializeField] Texture2D miningCursor;
     [SerializeField] Texture2D searchingCursor;
 
-    [SerializeField] CapsuleCollider2D playerCapsuleCollider;
-    [SerializeField] CompositeCollider2D rockCompositeCollider2D;
+    [SerializeField] CapsuleCollider2D playerCollider;
+    [SerializeField] CompositeCollider2D rockCollider;
 
-    //Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    void Start()
+    {
+        backpackBtn.onClick.AddListener(EquipPickaxe);
+
+    }
+
+    void EquipPickaxe()
+    {
+        gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
+    }
+
+
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
         {
             return;
         }
@@ -27,49 +43,18 @@ public class MiningAction : MonoBehaviour
         }
     }
 
-    //void Start()
-    //{
-    //    Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //}
 
-    void Update()
-    {
-        
-        EquipPickaxe();
-    }
 
-    public void EquipPickaxe()
-    {
-        //Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0) /*&& cursorPosition == backpack.transform.position*/)
-        {
-            Debug.Log("Backpack Accessed! Grab your pickaxe...");
-        }
-        
-    }
+
+
+
+
+
+
+    // check kenny if is touching rock: rockCompositeCollider2D.IsTouching(playerCapsuleCollider)
 
     // set to pickaxeCursor: gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
 
 
-    //void OnCollisionExit(Collision collision)
-    //{
-    //    gameCursorObject.GetComponent<GameCursor>().SetSearchingCursor(searchingCursor);
-    //}
-
-    //void OnMouseEnter()
-    //{
-    //    if (rockCompositeCollider2D.gameObject.tag == "rock")
-    //    {
-    //        gameCursorObject.GetComponent<GameCursor>().SetMiningCursor(miningCursor); // <GameCusor> name of other script; SetMiningCursor() method pulled from other script
-    //    }
-    //}
-
-    //void OnMouseExit()
-    //{
-    //    if (rockCompositeCollider2D.gameObject.tag == "rock")
-    //    {
-    //        gameCursorObject.GetComponent<GameCursor>().SetSearchingCursor(searchingCursor);
-    //    }
-    //}
 }
