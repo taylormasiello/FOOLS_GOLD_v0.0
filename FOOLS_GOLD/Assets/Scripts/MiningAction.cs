edacbad601d0f4c47382758a7b4096a6a6afc27b
Taylor Masiello <masiellotay@gmail.com>
2022-02-24 03:09:31 -0800

    Revert "cleaned up code"
    This reverts commit 034aeac61470bd30ff6d4afbf4a19e73c3989e2a.

--- FOOLS_GOLD/Assets/Scripts/MiningAction.cs
+++ FOOLS_GOLD/Assets/Scripts/MiningAction.cs
@@ -19,28 +19,19 @@ public class MiningAction : MonoBehaviour
 
     void Start()
     {
-        SetSearchingCursor(searchingCursor);
         backpackBtn.onClick.AddListener(EquipPickaxe);
 
     }
 
-    public void SetSearchingCursor(Texture2D texture)
-    {
-        Cursor.SetCursor(searchingCursor, Vector2.zero, CursorMode.Auto);
-    }
-
-    public void SetMiningCursor(Texture2D texture)
-    {
-        Cursor.SetCursor(miningCursor, Vector2.zero, CursorMode.Auto);
-    }
-
     void EquipPickaxe()
     {
-        SetMiningCursor(miningCursor);
-        /*gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor)*/;
+        gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
     }
 
-    void OnCollisionEnter2D(Collision2D collision) 
+
+
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
         {
