944fe24f953d172e442506b3a89569b0c1ddfb94
Taylor Masiello <masiellotay@gmail.com>
2022-02-24 00:24:32 -0800

    click on backpack_btn debug.log test functionality working

--- FOOLS_GOLD/Assets/Scripts/MiningAction.cs
+++ FOOLS_GOLD/Assets/Scripts/MiningAction.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class MiningAction : MonoBehaviour
 {
-    [SerializeField] GameObject gameCursorObject;
+    [SerializeField] GameObject gameCursor;
+    [SerializeField] GameObject backpack;
+
     [SerializeField] Texture2D miningCursor;
     [SerializeField] Texture2D searchingCursor;
 
     [SerializeField] CapsuleCollider2D playerCapsuleCollider;
     [SerializeField] CompositeCollider2D rockCompositeCollider2D;
+
+    //Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,10 +23,39 @@ public class MiningAction : MonoBehaviour
         }
         else
         {
-            gameCursorObject.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
-
+            Debug.Log("Kenny is trying to mine");
         }
     }
+
+    //void Start()
+    //{
+    //    Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //}
+
+    void Update()
+    {
+        
+        EquipPickaxe();
+    }
+
+    public void EquipPickaxe()
+    {
+        //Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) /*&& cursorPosition == backpack.transform.position*/)
+        {
+            Debug.Log("Backpack Accessed! Grab your pickaxe...");
+        }
+        
+    }
+
+    // set to pickaxeCursor: gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
+
+
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    gameCursorObject.GetComponent<GameCursor>().SetSearchingCursor(searchingCursor);
+    //}
 
     //void OnMouseEnter()
     //{
