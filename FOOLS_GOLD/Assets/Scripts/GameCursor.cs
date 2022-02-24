d554c7bea2a6c7554aea2fb319cc52a9f038f9fa
Taylor Masiello <masiellotay@gmail.com>
2022-02-24 03:17:47 -0800

    commit issues

--- FOOLS_GOLD/Assets/Scripts/GameCursor.cs
+++ FOOLS_GOLD/Assets/Scripts/GameCursor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameCursor : MonoBehaviour
 {
+<<<<<<< HEAD
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,28 @@ public class GameCursor : MonoBehaviour
     void Update()
     {
         
+=======
+    [SerializeField] Texture2D searchingCursorTexture;
+    [SerializeField] Texture2D miningCursorTexture;
+
+    //Cursor searchingCursor;
+    //Cursor miningCursor;
+
+    // Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+
+    void Start()
+    {
+        SetSearchingCursor(searchingCursorTexture);
+    }
+
+    public void SetSearchingCursor(Texture2D texture)
+    {
+        Cursor.SetCursor(searchingCursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void SetMiningCursor(Texture2D texture)
+    {
+        Cursor.SetCursor(miningCursorTexture, Vector2.zero, CursorMode.Auto);
+>>>>>>> parent of 034aeac (cleaned up code)
     }
 }
