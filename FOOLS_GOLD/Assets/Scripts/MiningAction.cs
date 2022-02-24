eb3c716e53dad5532f12120fea72f889a576af79
Taylor Masiello <masiellotay@gmail.com>
2022-02-24 10:18:47 -0800

    cleaned up code

--- FOOLS_GOLD/Assets/Scripts/MiningAction.cs
+++ FOOLS_GOLD/Assets/Scripts/MiningAction.cs
@@ -4,47 +4,80 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
+using UnityEngine.Tilemaps;
 
 public class MiningAction : MonoBehaviour
 {
-    [SerializeField] GameObject gameCursor;
-    [SerializeField] Button backpackBtn;
+    [SerializeField] Grid tilemapGrid;
+    [SerializeField] Tilemap rockTilemap;
 
-    [SerializeField] Texture2D miningCursor;
-    [SerializeField] Texture2D searchingCursor;
-
+    [SerializeField] GameObject player;
     [SerializeField] CapsuleCollider2D playerCollider;
-    [SerializeField] CompositeCollider2D rockCollider;
 
+    //Collision2D playerToMine;
+    ContactPoint2D[] playToRockContacts = new ContactPoint2D[2];
 
-    void Start()
+    //Kenny goes to mine
+    void OnCollisionEnter2D(Collision2D playerToMine)
     {
-        backpackBtn.onClick.AddListener(EquipPickaxe);
+        //makes grid from rocktilemap
+        Grid rockTileGrid = rockTilemap.layoutGrid;
 
+        //gets array of contact points from collision, makes List from array
+        playerToMine.GetContacts(playToRockContacts);
+        List<ContactPoint2D> contactPointsList = new List<ContactPoint2D>(playToRockContacts);
+
+        //stores both contactPoints from array into variables
+        ContactPoint2D collsionPt1 = contactPointsList[0];
+        ContactPoint2D collisionPt2 = contactPointsList[1];
+
+        //get cell coordiantes in which collision points are; converts world position to cell position
+        Vector3Int collisionCellPos1 = rockTileGrid.WorldToCell(collsionPt1.point);
+        Vector3Int collisionCellPos2 = rockTileGrid.WorldToCell(collisionPt2.point);
+
+        //gets TileBase from collision tile
+        TileBase collisionTile1 = rockTilemap.GetTile(collisionCellPos1);
+        TileBase collisionTile2 = rockTilemap.GetTile(collisionCellPos2);
+
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
+        {
+            return;
+        }
+        else if (contactPointsList != null)
+        {
+            Debug.Log("Kenny is mining");
+            foreach (var contact in playToRockContacts)
+            {
+                Debug.Log(contact.point);
+            }
+        }
     }
 
-    void EquipPickaxe()
+    //kenny is finished mining
+    void OnCollisionExit2D(Collision2D playerToMine)
     {
-        gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
+        Grid rockTileGrid = rockTilemap.layoutGrid;
+        playerToMine.GetContacts(playToRockContacts);
+        List<ContactPoint2D> contactPointsList = new List<ContactPoint2D>(playToRockContacts);
+
+        ContactPoint2D collsionPt1 = contactPointsList[0];
+        ContactPoint2D collisionPt2 = contactPointsList[1];
+
+        Vector3Int collisionCellPos1 = rockTileGrid.WorldToCell(collsionPt1.point);
+        Vector3Int collisionCellPos2 = rockTileGrid.WorldToCell(collisionPt2.point);
+
+        TileBase collisionTile1 = rockTilemap.GetTile(collisionCellPos1);
+        TileBase collisionTile2 = rockTilemap.GetTile(collisionCellPos2);
+
+        // collision.collider: find the collider of the gameobj your gameobj hits
+        if (playerToMine.collider.tag != "Rock")
+        {
+            return;
+        }
+        else if (contactPointsList != null)
+        {
+            Debug.Log("Kenny is finished mining");
+        }
     }
-
-    //void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
-    //    {
-    //        return;
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("kenny smacks rocks");
-    //    }
-    //}
-
-
-
-    // check kenny if is touching rock: rockCompositeCollider2D.IsTouching(playerCapsuleCollider)
-
-    // set to pickaxeCursor: gameCursor.GetComponent<GameCursor>().SetMiningCursor(miningCursor);
-
-
 }
--- FOOLS_GOLD/Assets/Scripts/TestTrial.cs
+++ FOOLS_GOLD/Assets/Scripts/TestTrial.cs
@@ -1,125 +1,118 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using UnityEngine.Events;
-using UnityEngine.UIElements;
-using UnityEngine.Tilemaps;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+//using UnityEngine.UI;
+//using UnityEngine.Events;
+//using UnityEngine.UIElements;
+//using UnityEngine.Tilemaps;
 
-public class TestTrial : MonoBehaviour
-{
-    [SerializeField] Grid tilemapGrid;
-    [SerializeField] Tilemap rockTilemap; 
+//public class TestTrial : MonoBehaviour
+//{
+//    [SerializeField] Grid tilemapGrid;
+//    [SerializeField] Tilemap rockTilemap; 
 
-    [SerializeField] GameObject player;
-    [SerializeField] CapsuleCollider2D playerCollider;
+//    [SerializeField] GameObject player;
+//    [SerializeField] CapsuleCollider2D playerCollider;
 
-    Collision2D playerToMine;
-    ContactPoint2D[] playToRockContacts = new ContactPoint2D[2];
-
-    
-
-    //Vector3 playerWorldPos;
-
-    //void GetPlayWorldPos()
-    //{
-    //    Vector3 playerWorldPos = player.GetComponent<PlayerMovement>().playerWorldPos;
-    //}
-
-    void OnCollisionEnter2D(Collision2D playerToMine)
-    {
-        //makes grid from rocktilemap
-        Grid rockTileGrid = rockTilemap.layoutGrid;
+//    //Collision2D playerToMine;
+//    ContactPoint2D[] playToRockContacts = new ContactPoint2D[2];
         
-        //gets array of contact points from collision, makes List from array
-        playerToMine.GetContacts(playToRockContacts);
-        List<ContactPoint2D> contactPointsList = new List<ContactPoint2D>(playToRockContacts);
+//    //Kenny goes to mine
+//    void OnCollisionEnter2D(Collision2D playerToMine)
+//    {
+//        //makes grid from rocktilemap
+//        Grid rockTileGrid = rockTilemap.layoutGrid;
+        
+//        //gets array of contact points from collision, makes List from array
+//        playerToMine.GetContacts(playToRockContacts);
+//        List<ContactPoint2D> contactPointsList = new List<ContactPoint2D>(playToRockContacts);
 
-        //stores both contactPoints from array into variables
-        ContactPoint2D collsionPt1 = contactPointsList[0];
-        ContactPoint2D collisionPt2 = contactPointsList[1];
+//        //stores both contactPoints from array into variables
+//        ContactPoint2D collsionPt1 = contactPointsList[0];
+//        ContactPoint2D collisionPt2 = contactPointsList[1];
 
-        //get cell coordiantes in which collision points are; converts world position to cell position
-        Vector3Int collisionCellPos1 = rockTileGrid.WorldToCell(collsionPt1.point);
-        Vector3Int collisionCellPos2 = rockTileGrid.WorldToCell(collisionPt2.point);
+//        //get cell coordiantes in which collision points are; converts world position to cell position
+//        Vector3Int collisionCellPos1 = rockTileGrid.WorldToCell(collsionPt1.point);
+//        Vector3Int collisionCellPos2 = rockTileGrid.WorldToCell(collisionPt2.point);
 
-        //gets TileBase from collision tile
-        TileBase collisionTile1 = rockTilemap.GetTile(collisionCellPos1);
-        TileBase collisionTile2 = rockTilemap.GetTile(collisionCellPos2);
+//        //gets TileBase from collision tile
+//        TileBase collisionTile1 = rockTilemap.GetTile(collisionCellPos1);
+//        TileBase collisionTile2 = rockTilemap.GetTile(collisionCellPos2);
 
-        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
-        {
-            return;
-        }
-        else if (contactPointsList != null)
-        {
-            Debug.Log("Kenny is mining");
-            foreach (var contact in playToRockContacts)
-            {
-                Debug.Log(contact.point);
-            }
-        }
-    }
+//        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
+//        {
+//            return;
+//        }
+//        else if (contactPointsList != null)
+//        {
+//            Debug.Log("Kenny is mining");
+//            foreach (var contact in playToRockContacts)
+//            {
+//                Debug.Log(contact.point);
+//            }
+//        }
+//    }
 
-    void OnCollisionExit2D(Collision2D playerToMine)
-    {
-        Grid rockTileGrid = rockTilemap.layoutGrid;
-        playerToMine.GetContacts(playToRockContacts);
-        List<ContactPoint2D> contactPointsList = new List<ContactPoint2D>(playToRockContacts);
+//    //kenny is finished mining
+//    void OnCollisionExit2D(Collision2D playerToMine)
+//    {
+//        Grid rockTileGrid = rockTilemap.layoutGrid;
+//        playerToMine.GetContacts(playToRockContacts);
+//        List<ContactPoint2D> contactPointsList = new List<ContactPoint2D>(playToRockContacts);
 
-        ContactPoint2D collsionPt1 = contactPointsList[0];
-        ContactPoint2D collisionPt2 = contactPointsList[1];
+//        ContactPoint2D collsionPt1 = contactPointsList[0];
+//        ContactPoint2D collisionPt2 = contactPointsList[1];
 
-        Vector3Int collisionCellPos1 = rockTileGrid.WorldToCell(collsionPt1.point);
-        Vector3Int collisionCellPos2 = rockTileGrid.WorldToCell(collisionPt2.point);
+//        Vector3Int collisionCellPos1 = rockTileGrid.WorldToCell(collsionPt1.point);
+//        Vector3Int collisionCellPos2 = rockTileGrid.WorldToCell(collisionPt2.point);
 
-        TileBase collisionTile1 = rockTilemap.GetTile(collisionCellPos1);
-        TileBase collisionTile2 = rockTilemap.GetTile(collisionCellPos2);
+//        TileBase collisionTile1 = rockTilemap.GetTile(collisionCellPos1);
+//        TileBase collisionTile2 = rockTilemap.GetTile(collisionCellPos2);
 
-        // (playerCollider.IsTouchingLayers(LayerMask.GetMask("Mining")))
-        if (playerToMine.collider.tag != "Rock") // collision.collider: find the collider of the gameobj your gameobj hits
-        {
-            return;
-        }
-        else if (contactPointsList != null)
-        {
-            Debug.Log("Kenny is finished mining");
-        }
-    }
+//        // collision.collider: find the collider of the gameobj your gameobj hits
+//        if (playerToMine.collider.tag != "Rock") 
+//        {
+//            return;
+//        }
+//        else if (contactPointsList != null)
+//        {
+//            Debug.Log("Kenny is finished mining");
+//        }
+//    }
 
-    void Pickaxe()
-    {
-        //Captures WORLD space for player and cursor
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 playerPos = player.GetComponent<PlayerMovement>().playerWorldPos;
+//    //void Pickaxe()
+//    //{
+//    //    //Captures WORLD space for player and cursor
+//    //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//    //    Vector3 playerPos = player.GetComponent<PlayerMovement>().playerWorldPos;
 
-        //Converts world space click point to tile map click point
-        Vector3Int mousePosTranslated = rockTilemap.WorldToCell(mousePos);
+//    //    //Converts world space click point to tile map click point
+//    //    Vector3Int mousePosTranslated = rockTilemap.WorldToCell(mousePos);
 
-        //Converts player world space to tile map space
-        Vector3Int playerPosTranslated = rockTilemap.WorldToCell(playerPos);
+//    //    //Converts player world space to tile map space
+//    //    Vector3Int playerPosTranslated = rockTilemap.WorldToCell(playerPos);
 
-        //Gives clicked on by cursor tile
-        TileBase clickedTile = rockTilemap.GetTile<TileBase>(mousePosTranslated);
+//    //    //Gives clicked on by cursor tile
+//    //    TileBase clickedTile = rockTilemap.GetTile<TileBase>(mousePosTranslated);
 
-        //Gives touched by Kenny tile
-        TileBase touchedtile = rockTilemap.GetTile<TileBase>(playerPosTranslated);
+//    //    //Gives touched by Kenny tile
+//    //    TileBase touchedtile = rockTilemap.GetTile<TileBase>(playerPosTranslated);
 
-        //(mousePosTranslated == playerPosTranslated) \\ (clickedTile == touchedtile)
+//        //(mousePosTranslated == playerPosTranslated) \\ (clickedTile == touchedtile)
 
 
 
-        //foreach (var contact in playToRockContacts)
-        //{
-        //    Debug.Log(contact.point); // each elm in arr is a point: (int x, int y)
-        //    // rockTileGrid.WorldToCell(contact.point); // cell coordinates in which collision point is
-        //    TileBase collisionTile = rockTilemap.GetTile(rockTileGrid.WorldToCell(contact.point));
+//        //foreach (var contact in playToRockContacts)
+//        //{
+//        //    Debug.Log(contact.point); // each elm in arr is a point: (int x, int y)
+//        //    // rockTileGrid.WorldToCell(contact.point); // cell coordinates in which collision point is
+//        //    TileBase collisionTile = rockTilemap.GetTile(rockTileGrid.WorldToCell(contact.point));
 
-        //}
+//        //}
 
-        //tilemapGrid.WorldToCell(playToRockContacts);
-        //rockTilemap.GetInstantiatedObject();
+//        //tilemapGrid.WorldToCell(playToRockContacts);
+//        //rockTilemap.GetInstantiatedObject();
 
-    }
-}
+//    //}
+//}
