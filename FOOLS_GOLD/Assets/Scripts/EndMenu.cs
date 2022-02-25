7d8ab57ef9923e0ae81e2cbd3439fa727315d146
Taylor Masiello <masiellotay@gmail.com>
2022-02-25 07:27:35 -0800

    Revert "bug fixes; all good except replay timer bug still NEEDS to be fixed;;"
    This reverts commit 47c68556add1a2f4a568c3a12be982998eefb581.

--- FOOLS_GOLD/Assets/Scripts/EndMenu.cs
+++ FOOLS_GOLD/Assets/Scripts/EndMenu.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class EndMenu : MonoBehaviour
 {
-    //[SerializeField] TextMeshProUGUI finalScoreText;
-    //ScoreKeeper scoreKeeper;
 
-    //void Awake()
-    //{
-    //    scoreKeeper = FindObjectOfType<ScoreKeeper>();
-    //}
+    // [SerializeField] TextMeshProUGUI finalScoreText; <- PH for scoreLogic after miningLogic
+    // [SerializeField] ScoreKeeper scoreKeeper; <- PH for scoreLogic after miningLogic
+
+    void Awake()
+    {
+        // scoreKeeper = FindObjectOfType<ScoreKeeper>(); <- PH for scoreLogic after miningLogic
+    }
+
+    public void ShowFinalScore()
+    {
+        // finalScoreText.text = "Congratulations!\nYou got a score of " + scoreKeeper.CalculateScore();
+        // PH for scoreLogic after miningLogic
+    }
 
 
-    //public void ShowFinalScore()
-    //{
-    //    finalScoreText.text = "Congrats!\nDid you find a lot of REAL GOLD?\nOr just FOOL'S GOLD?\nFinal Score: " + scoreKeeper.FinalScore();
-    //}
 }
--- FOOLS_GOLD/Assets/Scripts/GameManager.cs
+++ FOOLS_GOLD/Assets/Scripts/GameManager.cs
@@ -5,52 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] EndMenu endMenu;
+    [SerializeField] Canvas endMenu;
     [SerializeField] Canvas hud;
-    [SerializeField] GameTimer gameTimer;
-
-    void Awake()
-    {
-       endMenu = FindObjectOfType<EndMenu>();
-       hud = GameObject.Find("HUD").GetComponent<Canvas>();
-    }
 
     void Start()
     {
-        hud.gameObject.SetActive(true);
-        endMenu.gameObject.SetActive(false);        
+        endMenu.enabled = false;
+        hud.enabled = true;
     }
 
     void Update()
-    {
+    {   
         EscApp();
-        EndScreen();
     }
 
-    public void EndScreen()
+    public void OnReplyLevel()
     {
-        if (gameTimer.gameOver)
-        {
-            hud.gameObject.SetActive(false);
-            endMenu.gameObject.SetActive(true);
-            //endMenu.ShowFinalScore();
-        }
-    }
-
-    public void ReplyLevel()
-    {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
-        gameTimer.StartTimer();
-
-        // ***FIX REPLAY TIMER BUG** froze on replay 
-
-        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
-    }
-
-    public void TitleMenu()
-    {
-        SceneManager.LoadSceneAsync(0);
-        //SceneManager.UnloadSceneAsync(1, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+        SceneManager.LoadScene(0); 
     }
 
     public void QuitBtn()
